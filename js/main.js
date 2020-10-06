908e1351ddf876e92f600ce36dbce3f5c02971a4
Khotso <ktlibe01@louisville.edu>
2020-10-06 11:42:50 -0400

    update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@ function priPub() {
     q2.style.display = 'block'
 }
 
+document.getElementById('two').onchange = function() {
+    let two = document.getElementsByClassName("twoYear")[0]
+    two.style.display = 'block'
+
+    let input = document.getElementsByClassName("fourYear");
+    for ( let i = 0; i < input.length; i++) {
+        input[i].style.display = 'none'
+    }
+
+    const q2 = document.getElementById('q2')
+    q2.style.display = 'none'
+}
+
+document.getElementById('four').onchange = function () {
+    let input = document.getElementsByClassName("fourYear");
+    for ( let i = 0; i < input.length; i++) {
+        input[i].style.display = 'block'
+    }
+
+    let two = document.getElementsByClassName("twoYear")[0]
+    two.style.display = 'none'
+
+    const q2 = document.getElementById('q2')
+    q2.style.display = 'block'
+}
+
+
+
 function smallLarge() {
     const q3 = document.getElementById('q3')
     q3.style.display = 'block'
@@ -13,7 +41,13 @@ function liberalReligous() {
     q4.style.display = 'block'
 }
 
-const fourYearButton = document.getElementById('showFour')
+document.getElementById('clear-button').addEventListener('click', function() {
+    ["two", "four", "public", "private", "small", "large", "liberalArts", "religous"].forEach(function(id) {
+        document.getElementById(id).checked = false;
+    });
+});
+
+const fourYearButton = document.getElementById('four')
 const publicButton = document.getElementById('public')
 const largeButton = document.getElementById('large')
 const privateButton = document.getElementById('private')
