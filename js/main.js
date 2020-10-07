604d33d40c65776173ab0a36b804a257dddd5d68
Khotso <ktlibe01@louisville.edu>
2020-10-07 11:19:55 -0400

    button functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,73 @@
-// function priPub() {
-//     const q2 = document.getElementById('q3')
-//     q2.style.display = 'block'
-// }
 
+// Show small colleges if button is selected and don't show them if the button isn't selected
+function smallButton() {
+    if ( document.getElementById('small').checked == true ) {
+        let smallColl = document.getElementsByClassName("small");
+        for ( let i = 0; i < smallColl.length; i++) {
+                smallColl[i].style.display = 'block'
+        }
+    }
+
+    if ( document.getElementById('small').checked == false ) {
+        let smallColl = document.getElementsByClassName("small");
+        for ( let i = 0; i < smallColl.length; i++) {
+                smallColl[i].style.display = 'none'
+        }
+    }
+}
+
+// Show large colleges if button is selected and don't show them if the button isn't selected
+function largeButton() {
+    if ( document.getElementById('large').checked == true ) {
+        let largeColl = document.getElementsByClassName("large");
+        for ( let i = 0; i < largeColl.length; i++) {
+                largeColl[i].style.display = 'block'
+        }
+    }
+
+    if ( document.getElementById('large').checked == false ) {
+        let largeColl = document.getElementsByClassName("large");
+        for ( let i = 0; i < largeColl.length; i++) {
+                largeColl[i].style.display = 'none'
+        }
+    }
+}
+
+// Show private colleges if button is selected and don't show them if the button isn't selected
+function privateButton() {
+    if ( document.getElementById('private').checked == true ) {
+        let privateColl = document.getElementsByClassName("private");
+        for ( let i = 0; i < privateColl.length; i++) {
+                privateColl[i].style.display = 'block'
+        }
+    }
+
+    if ( document.getElementById('private').checked == false ) {
+        let privateColl = document.getElementsByClassName("private");
+        for ( let i = 0; i < privateColl.length; i++) {
+                privateColl[i].style.display = 'none'
+        }
+    }
+}
+
+// Show public colleges if button is selected and don't show them if the button isn't selected
+function publicButton() {
+    if ( document.getElementById('public').checked == true ) {
+        let publicColl = document.getElementsByClassName("public");
+        for ( let i = 0; i < publicColl.length; i++) {
+                publicColl[i].style.display = 'block'
+        }
+    }
+
+    if ( document.getElementById('public').checked == false ) {
+        let publicColl = document.getElementsByClassName("public");
+        for ( let i = 0; i < publicColl.length; i++) {
+                publicColl[i].style.display = 'none'
+        }
+    }
+}
+
+// Question1
 // Show 2-year colleges and hide the others
 document.getElementById('two').onchange = function() {
     let two = document.getElementsByClassName("twoYear")[0]
@@ -37,9 +102,13 @@ document.getElementById('four').onchange = function () {
     q2.style.display = 'block'
 }
 
-
+// Question2
 // Show 4-Year Small colleges
 document.getElementById('small').onchange = function(){
+    smallButton()
+    largeButton()
+
+    
     let smallColl = document.getElementsByClassName("small");
     for ( let i = 0; i < smallColl.length; i++) {
         smallColl[i].style.display = 'block'
@@ -52,10 +121,15 @@ document.getElementById('small').onchange = function(){
 
     const q3 = document.getElementById('q3')
     q3.style.display = 'block'
+
+
 }
 
 // Show 4-year Large colleges
 document.getElementById('large').onchange = function(){
+    smallButton()
+    largeButton()
+    
     let largeColl = document.getElementsByClassName("large");
     for ( let i = 0; i < largeColl.length; i++) {
         largeColl[i].style.display = 'block'
@@ -68,11 +142,18 @@ document.getElementById('large').onchange = function(){
 
     const q3 = document.getElementById('q3')
     q3.style.display = 'block'
+
 }
 
-
+// Question3
 // Show 4-year Private colleges
 document.getElementById('private').onchange = function(){
+
+    privateButton()
+    publicButton()
+    smallButton()
+    largeButton()
+
     let privateColl = document.getElementsByClassName("private");
     for ( let i = 0; i < privateColl.length; i++) {
         privateColl[i].style.display = 'block'
@@ -93,10 +174,16 @@ document.getElementById('private').onchange = function(){
     });
     
 
+
 }
 
 // Show 4-year Public colleges
 document.getElementById('public').onchange = function(){
+    privateButton()
+    publicButton()
+    smallButton()
+    largeButton()
+
     let publicColl = document.getElementsByClassName("public");
     for ( let i = 0; i < publicColl.length; i++) {
         publicColl[i].style.display = 'block'
@@ -120,8 +207,10 @@ document.getElementById('public').onchange = function(){
     const q4 = document.getElementById('q4')
     q4.style.display = 'none'
 
+
 }
 
+// Question4
 // Show Liberal Arts Private Colleges
 document.getElementById('liberalArts').onchange = function(){
     let libColl = document.getElementsByClassName("liberalArts");
@@ -160,36 +249,10 @@ document.getElementById('religous').onchange = function(){
 
 
 
-// function smallLarge() {
-//     const q3 = document.getElementById('q2')
-//     q3.style.display = 'block'
-// }
-
-
-// function liberalReligous() {
-//     const q4 = document.getElementById('q4')
-//     q4.style.display = 'block'
-// }
-
-
 // Resetting the radio buttons
 document.getElementById('clear-button').addEventListener('click', function() {
     ["two", "four", "public", "private", "small", "large", "liberalArts", "religous"].forEach(function(id) {
         document.getElementById(id).checked = false;
     });
 });
-
-// const fourYearButton = document.getElementById('four')
-// const publicButton = document.getElementById('public')
-// const largeButton = document.getElementById('large')
-// const privateButton = document.getElementById('private')
-// const smallButton = document.getElementById('small')
-
-
-
-// fourYearButton.addEventListener('click', priPub)
-// publicButton.addEventListener('click', smallLarge)
-// largeButton.addEventListener('click', liberalReligous)
-// privateButton.addEventListener('click', smallLarge)
-// smallButton.addEventListener('click', liberalReligous)
 
